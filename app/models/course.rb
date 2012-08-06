e008cae884b5f04c526b0ec3f00fa6aef670f707
Aayush Dawra <aayushgopaldawra@gmail.com>
2012-08-05 23:58:47 -0700

    Added Search box for Research Papers.

--- app/models/course.rb
+++ app/models/course.rb
@@ -84,5 +84,12 @@ class Course < ActiveRecord::Base
     end
   end
   
+  def self.search(search)
+    if search
+      where('name LIKE ?', "%#{search}%")
+    else
+      scoped
+    end
+  end
 
 end
